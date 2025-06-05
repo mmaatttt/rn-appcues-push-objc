8331d262122b3c4dbbb4b3e44927ab72dd9c3a02
Matt Hayashida <matt.hayashida@appcues.com>
2025-06-05 11:49:38 -0400

    add existing push infrastructure

--- ios/AwesomeProject/AppDelegate.mm
+++ ios/AwesomeProject/AppDelegate.mm
@@ -1,6 +1,10 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <UserNotifications/UserNotifications.h>
+
+@interface AppDelegate () <UNUserNotificationCenterDelegate>
+@end
 
 @implementation AppDelegate
 
@@ -10,6 +14,10 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  center.delegate = self;
+  [application registerForRemoteNotifications];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
@@ -26,6 +34,30 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       didReceiveNotificationResponse:(UNNotificationResponse *)response
+             withCompletionHandler:(void (^)(void))completionHandler {
+
+    completionHandler();  // If not handled by Appcues, complete the handler
+}
+
+// Called when a notification is delivered to a foreground app.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
+{
+  // Decide if and how the notification will be shown to the user
+  if (@available(iOS 14.0, *)) {
+      completionHandler(UNNotificationPresentationOptionBanner | UNNotificationPresentationOptionList);  // iOS 14+
+  } else {
+      completionHandler(UNNotificationPresentationOptionAlert);  // Older iOS versions
+  }
 }
 
 @end
